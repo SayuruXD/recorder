62ab4ea2c043ba73871f8d817ca7c71068d6ab46
Sayuru Sathsara <sathsara12435.slk@gmail.com>
2026-08-21 16:29:51 +0530

    cleanup: remove unused FFmpeg resolver declaration

--- src/recorder.h
+++ src/recorder.h
@@ -28,7 +28,6 @@ private:
     bool LaunchEncoder(HWND targetWindow, const std::wstring& outputPath, const RecordingConfig& config, bool qsv);
     bool IsProcessAlive() const;
     void CloseProcessHandles();
-    std::wstring FindFfmpeg(std::wstring& detail) const;
 
     HANDLE process_ = nullptr;
     HANDLE stdinWrite_ = nullptr;
